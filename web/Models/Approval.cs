5ce20ae37c72115215aea081c0e4b320dec61ea6
Umair Anwar <umair.anwr@gmail.com>
2024-02-22 16:13:58 +0500

    Add model Approval

---
+++ web/Models/Approval.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Approval
+{
+    public int Id { get; set; }
+    public string? District { get; set; }
+    public string? Mineral { get; set; }
+    public string? TitleOfLease { get; set; }
+    public int ReservePrice { get; set; }
+    public int BidMoney { get; set; }
+    public int FinancialImpact { get; set; }
+    public string? NatureOfCase { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime DateOfApprovalRenewal { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime DateOfExpiry { get; set; }
+    public string? CSRS { get; set; }
+    public string? Status { get; set; }
+    public string? Remarks { get; set; }
+
+}
